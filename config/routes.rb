133a36c5eb68742133d4fb7c95cd4f982bc7703e
CristianRichie <cristianassaiante@outlook.com>
2019-05-27 19:03:11 +0200

    layout - devise signup migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks", registrations: 'home'}
     
     get '/home', to: 'home#index'
     get '/login', to: 'login#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_142804) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_142804) do
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
