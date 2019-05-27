d7caa04f304fccd4aaf9ec4012e23593cfdb0456
CristianRichie <cristianassaiante@outlook.com>
2019-05-27 22:31:59 +0200

    homepage - tweets and layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
     
+    $twitter_client = Twitter::REST::Client.new do | config |
+        config.consumer_key = Rails.application.secrets.twitter_api_key
+        config.consumer_secret = Rails.application.secrets.twitter_secret_key
+    end
+    
     def index
-        @tweets = []
+        @tweets =  $twitter_client.search("#ctf", result_type: "recent", lang: "en").first(1)
+        @tweets += $twitter_client.search("#defcon", result_type: "recent", lang: "en").first(2)
+        @tweets += $twitter_client.search("#blackhat", result_type: "recent", lang: "en").first(2)
     end
     
     def create
