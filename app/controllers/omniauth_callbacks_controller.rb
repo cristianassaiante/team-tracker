2f36c9347fe5a2b87cdb0beeb457cc2a0366d673
Greyb0w1er <imperatore.1758849@studenti.uniroma1.it>
2019-06-01 19:54:51 +0200

    oauth - google auth provider bug fixed

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -12,7 +12,7 @@ class OmniauthCallbacksController < ApplicationController
 
   def google_oauth2
     auth = request.env["omniauth.auth"]
-    user = User.where(provider: auth.provider, uid: auth.uid)
+    user = User.where(provider: 'google', uid: auth.uid)
                .first_or_create(email: auth.info.email, password: Devise.friendly_token[0,20], username: auth.info.name, provider: 'google')
 
     sign_in(:user, user)
